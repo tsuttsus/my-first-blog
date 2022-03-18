dea0e2ab0122fa14b1323a1bdd2de7142ce18624
Shunsuke Tsutsumi <brvotsuttsu@gmail.com>
2022-03-18 11:12:18 +0900

    Add GA Settings

---
+++ mysite/context_processors.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+
+def google_analytics(request):
+    ga_tracking_id=getattr(setting,'GOOGLE_ANALYTICS_TRACKING_ID',False)
+
+    if not settings.DEBUG and ga_tracking_id:
+        return {
+            'GOOGLE_ANALYTICS_TRACKING_ID':ga_tracking_id,
+        }
+    return {}
